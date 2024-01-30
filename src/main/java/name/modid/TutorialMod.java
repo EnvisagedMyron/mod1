6a209d1688b6540cfdb54a790ac85b6ce8ff9f40
BuildTools <unconfigured@null.spigotmc.org>
2024-01-30 11:07:25 +0200

    Initial commit

---
+++ src/main/java/name/modid/TutorialMod.java
@@ -0,0 +1,18 @@
+package name.modid;
+
+import name.modid.item.ModItems;
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class TutorialMod implements ModInitializer {
+	public static final String MOD_ID = "tutorialmod";
+	public static final Logger LOGGER = LoggerFactory.getLogger("mod1");
+
+	@Override
+	public void onInitialize() {
+
+		ModItems.registerModItems();
+	}
+}
---
+++ src/main/java/name/modid/item/ModItems.java
@@ -0,0 +1,34 @@
+package name.modid.item;
+
+import name.modid.TutorialMod;
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroupEntries;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.fabricmc.fabric.impl.itemgroup.FabricItemGroup;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroups;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
+
+public class ModItems {
+    // creating a ruby
+    public static final Item RUBY = registerItem("ruby",new Item(new FabricItemSettings()));
+
+    // adding the rub entry to ingedients tab
+    private static void addItemsToIngredientTabItemGroup(FabricItemGroupEntries entries){
+        entries.add(RUBY);
+    }
+
+
+    // registering the item
+    private static Item registerItem (String name,Item item){
+        return Registry.register(Registries.ITEM,new Identifier(TutorialMod.MOD_ID,name),item);
+    }
+    // registering the mod items and adding them to their desired tab
+    public static void registerModItems() {
+        TutorialMod.LOGGER.info("Registering Mod Items for " + TutorialMod.MOD_ID);
+
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register(ModItems::addItemsToIngredientTabItemGroup);
+    }
+}
